eb618e9db132648a1e73ec1f4d4fc14eb08d0310
Victória Finzi <36055318+victoriafinzi@users.noreply.github.com>
2022-08-31 14:10:37 -0300

    Delete post-comments directory

--- post-comments/app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(basedir, "db.sqlite")}'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = True
